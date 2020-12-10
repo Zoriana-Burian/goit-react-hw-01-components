b02f9078ce8b1aa1337704f9d870244ec7224577
Zoriana-Burian <ke7sha1991@gmail.com>
2020-12-10 22:48:25 +0200

    ..

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from '../src/components/App';
+//import './index.css';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//ReactDOM.render(
+//  <React.StrictMode>
+//    <App />
+//  </React.StrictMode>,
+//  document.getElementById('root')
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
+
+
+
+
+//<Profile 
+  //name={user.name}
+//  tag={user.tag}
+//  location={user.location}
+//  avatar={user.avatar}
+//  stats={user.stats}
+ // statsFolovers={user.stats.folovers}
+//  statsViews={user.stats.views}
+//  statsLikes={user.stats.likes} />
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>, 
+  document.querySelector('#root'));
+
